4c812e2eeed4f7c35355af680bb9b01643b8adbe
subigya <subigyapok08@gmail.com>
2026-07-23 14:39:56 +0545

    feat:hoisting

---
+++ hoisting.js
@@ -0,0 +1,13 @@
+// hoisting is JS behaviour which allocates memory for variables, functions at first and then assigns value to it
+
+// how we write a code
+
+let x=10;
+console.log(x);
+
+
+//how js runs the code
+
+let x; //allocates memory
+console.log(x); //undefined
+x=10; //assigns value into memory
