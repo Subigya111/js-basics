b71c33b5b6f842a0d408f3c8d796ffa531e95fae
subigya <subigyapok08@gmail.com>
2026-07-24 10:16:41 +0545

    feat:basics of object & this keyword

--- object.js
+++ object.js
@@ -1,4 +1,4 @@
-//non-primitive datatype which stores multiple values like string,integer
+// non-primitive datatype which stores multiple values like string,integer
 
 const users={
     name:"Raju",
@@ -11,3 +11,22 @@ console.log(users["name"]); //can be accessed using square brackets
 let nickname="name"; //name is property 
 console.log(users[nickname]); 
 console.log(users.nickname); //undefined
+
+
+/*this keyword refers to the object that is currently calling the function. Using object's name to call the function limits the function to
+only one object*/
+
+function greet(){
+    console.log("Hello "+ this.name);
+
+}
+const user1={
+    name:"Hari",
+    greet   //function name
+};
+const user2={
+    name:"Ramu",
+    greet
+}
+user1.greet();  //same function works for both objects because 'this' changes depending on which object called it.
+user2.greet();
