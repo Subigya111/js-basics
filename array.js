6983fd7d76b716db328274730ce2cc4339b7eab7
subigya <subigyapok08@gmail.com>
2026-07-26 09:54:39 +0545

    feat:add arrow function for map,reduce and filter methods

--- array.js
+++ array.js
@@ -16,16 +16,37 @@ let sub5=numbers.map(function(number){  //map() is used to create new array by a
 });
 console.log(sub5);
 
+let nums=[11,22,33,44,55,66];
+let divBy11=nums.map((num)=>{ //each number in nums array is 'num'
+    return num/11
+}); 
+console.log(divBy11); //returns empty array because with '{ }' use 'return' keyword
+let nums1=nums.map()
+
 
 let numbers1=[4,6,7,32,3];
 let evenNum=numbers1.filter(function(number){  //filter() is used to create a new array containing only elements where certain condition matches
     return (number%2==0); 
 });
-console.log(evenNum);
+console.log(evenNum);  // more in arrowFunction.js file
 
 
-const numbers=[10,20,30,40,50];
-const total=numbers.reduce(function(sum,num){  //reduce() is used when you want to take all elements from an array and combine them into a single value
-    return sum+num;
-});
+const numbers=[10,20,30];
+const total=numbers.reduce(function(accumulator,currentvalue){ // iteration 1: acc=10, curval=20  
+    return accumulator+currentvalue; /*'return' becomes next value of accumulator and currentvalue moves forward in array
+                                     iteration 2: acc=30, curval=30 */   
+
+}); // no initial value is set. so the value of acc is the first element of the array, i.e. 10. or else acc= inital value, curval= 1st element of array
+console.log(total); //60
+
+const numbers=[1,23,4];
+const total=numbers.reduce((accu,curval)=>accu+curval,0); //here initial value is 0. iteration 1: accu=0, curval=1. iteration 2: accu=1, curval=23 
+console.log(total); //make habit of writing intial value
+
+const shoppingCart=[  //object inside array
+    {itemName:"shoes",price:1500},
+    {itemName:"trimmer",price:5000},
+    {itemName:"laptopStand",price:500},
+];
+const total=shoppingCart.reduce((acc,item)=>acc+item.price,0); //provide initial value because the value of acc will be an entire object instead of price. 
 console.log(total);
--- arrowFunction.js
+++ arrowFunction.js
@@ -1,15 +1,3 @@
-const user={
-    name:"rani",
-    age:40,
-    greet(){
-        console.log(this);
-    }
-}
-user.greet(); //output is { name: 'rani', age: 40, greet: [Function: greet] }
-user.name="kajol"; 
-user.greet(); //output is { name: 'kajol', age: 40, greet: [Function: greet] }
-
-
 //normal function syntax
 function functionName(parameters){
     //code
@@ -27,7 +15,28 @@ console.log(add5(3,5));
 const add5=(num1,num2)=> (num1+num2);
 console.log(add5(3,5));
 
-//example with filter() method
+// example with filter() method
 const numbers=[12,3,45,64,6,31,18];
-const lessThan20=numbers.filter(number =>  number<=20); //filters numbers less than 20
+const lessThan20=numbers.filter((number)=>  number<=20); //filters numbers less than 20
 console.log(lessThan20);
+
+const moreThan20=numbers.filter((number)=>{
+     number >= 20        //returns empty array because with '{ }' use 'return' keyword
+});
+console.log(moreThan20);
+
+const books = [
+    { id: 1, bookName: "Atomic Habits", genre: "Self-Help",publishDate: "2018" },
+
+    { id: 2, bookName: "Deep Work", genre: "Self-Help",publishDate: "2016" },
+
+    { id: 3, bookName: "The Hobbit", genre: "Fantasy",publishDate: "1937" },
+
+    { id: 4, bookName: "Harry Potter and the Sorcerer's Stone", genre: "Fantasy",publishDate: "1997" },
+
+    { id: 5, bookName: "Clean Code", genre: "Programming",publishDate: "2008" },
+
+    { id: 6, bookName: "You Don't Know JS", genre: "Programming",publishDate: "2015" }
+];
+const oneBook= books.filter((book)=>book.publishDate >= 2000 && book.genre==='Self-Help');
+console.log(oneBook);
